763c7b1feaf1d1178fa91df9bf5fc2a29a2e3365
Antonis Kalou <kalouantonis@protonmail.com>
2023-11-27 16:14:16 +0200

    Finish moving to core

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,11 @@
 open Core
+module Unix = Core_unix
+module Sys = Sys_unix
 
 let rec find_parent_directory_with_file target_file start_dir =
   let target_file = Filename.concat start_dir target_file
   in
-    if Stdlib.Sys.is_directory target_file then
+    if Sys.is_directory_exn target_file then
       start_dir
     else
       (* Move one dir up and try again *)
@@ -11,7 +13,7 @@ let rec find_parent_directory_with_file target_file start_dir =
       |> find_parent_directory_with_file target_file
 
 let parent_dir () =
-  let start = Stdlib.Sys.getcwd ()
+  let start = Core_unix.getcwd ()
   (* TODO: make env variable with default *)
   in let target = "issue"
   in find_parent_directory_with_file target start
@@ -22,14 +24,15 @@ let issue_dir () =
   in Filename.concat dir "issue"
 
 let rec traverse_directory path =
-  if Stdlib.Sys.is_directory path then
-    Stdlib.Sys.readdir path
+  if Sys.is_directory_exn path then
+    Sys.readdir path
     |> Array.to_list
     |> List.concat_map ~f:(fun file ->
         Filename.concat path file
         |> traverse_directory
-      )
-  else [path]
+    )
+  else
+    [path]
 
 let wrap_in_article issue_html =
   "<article>" ^ issue_html ^ "</article>"
@@ -53,7 +56,9 @@ let to_html root =
 let list root =
   traverse_directory root
   |> List.filter ~f:(fun file ->
-      file |> Stdlib.Filename.extension |> String.lowercase |> String.equal ".md"
+      Filename.split_extension file
+      |> Tuple2.get2
+      |> Option.for_all ~f:(fun ext -> ext |> String.lowercase |> String.equal ".md")
     )
   |> List.iter ~f:(fun file -> print_endline file)
 
@@ -61,22 +66,13 @@ let safe_filename filename =
   let r = Str.regexp "[^A-Za-z0-9.-]" in
   Str.global_replace r "_" filename
 
-let rec mkdir_p path = 
-  if not (Stdlib.Sys.file_exists path) then begin
-    let parent = Filename.dirname path in
-    if not (String.equal path parent) then begin
-      mkdir_p parent
-    end;
-    Stdlib.Sys.mkdir path 0o777
-  end
-
-let move from to' = Stdlib.Sys.rename from to'
+let move from to' = Core_unix.rename ~src:from ~dst:to'
 
 let open_file_with_editor path =
-  let editor = Sys.getenv("EDITOR") |> Option.value ~default:"vi" in
+  let editor = Core.Sys.getenv("EDITOR") |> Option.value ~default:"vi" in
   (* open the temporary file with the default editor *)
-  Printf.sprintf "%s %s" editor path
-  |> Stdlib.Sys.command
+  Printf.sprintf "%s '%s'" editor path
+  |> Sys.command
   |> ignore
 
 let find_unique_filename path =
@@ -84,7 +80,7 @@ let find_unique_filename path =
   let r = Str.regexp "\\.md" in
   let counter = ref 1 in
   let search = ref path in
-  while Stdlib.Sys.file_exists !search do
+  while Sys.file_exists_exn !search do
     print_endline ("Possible duplicate issue found:\t" ^ !search);
     let replacement = Printf.sprintf "_%i.md" !counter in
     search := Str.replace_first r replacement path;
@@ -94,33 +90,29 @@ let find_unique_filename path =
 
 let open_issue () = 
   (* will create the file *)
-  let tmpfile = Stdlib.Filename.temp_file ~temp_dir:"issue" "tmp-" ".md" in
+  let tmpfile = Filename_unix.temp_file ~in_dir:"issue" "tmp-" ".md" in
   let open_dir = Filename.concat "issue" "open" in
   (* create the issue/open directory if it doesn't exit *)
-  ignore (mkdir_p open_dir);
+  ignore (Unix.mkdir_p open_dir);
   open_file_with_editor tmpfile;
-  (* check if the file was saved. FIXME: completely pointless since file is
-     guaranteed to exist by Filename.temp_file *)
-  if Stdlib.Sys.file_exists tmpfile then
-    let lines = lines_of_file tmpfile in
-    (* extract the title from the contents (first line) *)
-    (* TODO: add same regex check as in perl *)
-    match List.hd lines with
-    | Some title -> 
-        let title = title |> Stdlib.String.trim in
-        let issue_file = (title |> safe_filename) ^ ".md" in
-        let path = Filename.concat open_dir issue_file in
-        (* check for filename conflicts and find a unique filename *)
-        let unique_path = find_unique_filename path in
-        Printf.printf "Moving %s to %s.\n" tmpfile unique_path;
-        (* TODO: error handling *)
-        ignore (move tmpfile unique_path);
-        Printf.printf "Issue saved at: %s\n" unique_path;
-    | None -> 
-      (* TODO: delete file, since it hasn't been written to *)
-      print_endline "Cannot create an issue without a title.\n"
-  else
-    print_endline "No changes were saved.\n"
+  let lines = lines_of_file tmpfile in
+  (* extract the title from the contents (first line) *)
+  (* TODO: add same regex check as in perl *)
+  match List.hd lines with
+  | Some title -> 
+      let title = title |> String.strip in
+      let issue_file = (title |> safe_filename) ^ ".md" in
+      let path = Filename.concat open_dir issue_file in
+      (* check for filename conflicts and find a unique filename *)
+      let unique_path = find_unique_filename path in
+      Printf.printf "Moving %s to %s.\n" tmpfile unique_path;
+      (* TODO: error handling *)
+      ignore (move tmpfile unique_path);
+      Printf.printf "Issue saved at: %s\n" unique_path;
+  | None -> 
+    print_endline "No changes were saved.";
+    (* cleanup empty tempfile *)
+    Sys.remove tmpfile
 
 (* ENTRYPOINT *)
 (* let () = issue_dir () |> to_html |> print_endline *)
