0cb53ff4933ae86fbe35acd6a0045cdd33fb3464
bas081 <bas080@hotmail.com>
2023-12-03 02:09:37 +0100

    Reduce amount of listener calls in state helper

--- browser/state.test.mjs
+++ browser/state.test.mjs
@@ -1,43 +1,82 @@
-import { tap, isNotNil } from './helpers.mjs'
-import { test } from 'tape';
-import { check, gen } from 'tape-check';
-import state from './state.mjs'
+import { tap, isNotNil } from "./helpers.mjs";
+import { test } from "tape";
+import { check, gen } from "tape-check";
+import state from "./state.mjs";
 
-test('Calls listener on creation', check(gen.any, (t, value) => {
-  t.plan(3)
+test(
+  "Calls listener on creation",
+  check(gen.any, (t, value) => {
+    t.plan(3);
 
-  const onState = state(() => {
-    t.pass('Is being called on init')
-    return value
-  })
+    const onState = state(() => {
+      t.pass("Is being called on init");
+      return value;
+    });
 
-  onState(tap(state => t.equals(state, value, 'The value set in init is available in listener')))
-}))
+    onState(
+      tap((state) =>
+        t.equals(
+          state,
+          value,
+          "The value set in init is available in listener",
+        ),
+      ),
+    );
+  }),
+);
 
-test('Computed state should stay updated', check(gen.int, (t, int) => {
-  t.plan(11)
+test(
+  "Computed state should stay updated",
+  check(gen.int, (t, int) => {
+    t.plan(13);
 
-  const onState = state(() => {
-    return {
-      value: int
-    }
-  })
+    const onState = state(() => ({
+      value: int,
+    }));
 
-  // Should eventually include the computed property
-  onState(tap(state => {
-    t.equals(state.value, int, 'Value is equal to the test value')
+    // Should eventually include the computed property
+    onState(
+      tap((state) => {
+        t.equals(state.value, int, "Value is equal to the test value");
 
-    if (isNotNil(state.computed)) {
-      t.equals(int + 1, state.computed, 'Computed is one more than int eventually')
-    }
-  }))
+        if (isNotNil(state.computed)) {
+          t.equals(
+            int + 1,
+            state.computed,
+            "Computed is one more than int eventually",
+          );
+        }
+      }),
+    );
 
-  onState(state => ({
-    ...state,
-    computed: state.value + 1,
-  }))
+    onState((state) => ({
+      ...state,
+      computed: state.value + 1,
+    }));
 
-  onState(tap(state => {
-    t.equals(int + 1, state.computed, 'Computed is one more than int eventually')
-  }))
-}))
+    onState(
+      tap((state) => {
+        t.equals(
+          int + 1,
+          state.computed,
+          "Computed is one more than int eventually",
+        );
+      }),
+    );
+
+    onState((state, push) => {
+      setTimeout(
+        () =>
+          push(
+            tap((state) => {
+              t.pass("Push can be called even when listener is stopped");
+            }),
+          ),
+        10,
+      );
+      push();
+
+      return state;
+    });
+  }),
+);
