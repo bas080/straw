6f038ce3bab8a37e258fdbe90d0e0d114b4847ec
Antonis Kalou <kalouantonis@protonmail.com>
2023-12-08 13:49:07 +0200

    Open Omd in Omd_ext

--- lib/issue_lib/omd_ext.mli
+++ lib/issue_lib/omd_ext.mli
@@ -1,67 +1,69 @@
 (* Extensions to Omd, the markdown parser *)
 
+open Omd
+
 module Block : sig
     (** map over the inline elements in a block. *)
     val map :
-        f:('a Omd.inline -> 'a Omd.inline)
-        -> 'a Omd.block
-        -> 'a Omd.block
+        f:('a inline -> 'a inline)
+        -> 'a block
+        -> 'a block
 
     (** iterate over the inline elements of a block. *)
     val iter :
-        f:('a Omd.inline -> unit)
-        -> 'a  Omd.block
+        f:('a inline -> unit)
+        -> 'a  block
         -> unit
 
     val find :
-        f:('a Omd.inline -> bool)
-        -> 'a Omd.block
-        -> 'a Omd.inline option
+        f:('a inline -> bool)
+        -> 'a block
+        -> 'a inline option
 
     (* val find_map :
-        f:('a Omd.inline -> 'b option)
-        -> 'a Omd.block
+        f:('a inline -> 'b option)
+        -> 'a block
         -> 'b option *)
 end
 
 module Document : sig
     (** map over each block in a document *)
-    val map : f:(Omd.attributes Omd.block -> Omd.attributes Omd.block) -> Omd.doc -> Omd.doc
+    val map : f:(attributes block -> attributes block) -> doc -> doc
 
     (** iterate over each block in a document *)
-    val iter: f:(Omd.attributes Omd.block -> unit) -> Omd.doc -> unit
+    val iter: f:(attributes block -> unit) -> doc -> unit
 
     (** attempt to find an element that matches the given predicate *)
     val find :
-        f:(Omd.attributes Omd.block -> bool)
-        -> Omd.doc
-        -> Omd.attributes Omd.block option
+        f:(attributes block -> bool)
+        -> doc
+        -> attributes block option
 
     (* val find_map :
-        f:(Omd.attributes Omd.block -> 'b option)
-        -> Omd.doc
+        f:(attributes block -> 'b option)
+        -> doc
         -> 'b option *)
 end
 
-(** Same as Document.map ~f:(Block.map ~f), but also handles Omd.Concat types. *)
+(** Same as Document.map ~f:(Block.map ~f), but also handles Concat types. *)
 val inline_map :
-    f:(Omd.attributes Omd.inline -> Omd.attributes Omd.inline)
-    -> Omd.doc
-    -> Omd.doc
+    f:(attributes inline -> attributes inline)
+    -> doc
+    -> doc
 
-(** Same as Document.iter ~f:(Block.iter ~f), but also handles Omd.Concat types. *)
+(** Same as Document.iter ~f:(Block.iter ~f), but also handles Concat types. *)
 val inline_iter :
-    f:(Omd.attributes Omd.inline -> unit)
-    -> Omd.doc
+    f:(attributes inline -> unit)
+    -> doc
     -> unit
 
 val inline_find :
-    f:(Omd.attributes Omd.inline -> bool)
-    -> Omd.doc
-    -> Omd.attributes Omd.inline option
+    f:(attributes inline -> bool)
+    -> doc
+    -> attributes inline option
 
 (* FIXME: type signatures are completely borked *)
 (* val inline_find_map :
-    f:('a Omd.inline -> 'b option)
-    -> Omd.doc
+    f:('a inline -> 'b option)
+    -> doc
     -> 'b option *)
