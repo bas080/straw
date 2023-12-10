ef0493632f9f128ec8a09d8503274abb926da5e7
bas081 <bas080@hotmail.com>
2023-12-10 19:19:55 +0100

    Make the browser code work for OCaml HTML output

--- browser/state.test.mjs
+++ browser/state.test.mjs
@@ -1,82 +1,82 @@
-import { tap, isNotNil } from "./helpers.mjs";
-import { test } from "tape";
-import { check, gen } from "tape-check";
-import state from "./state.mjs";
+import { tap, isNotNil } from './helpers.mjs'
+import { test } from 'tape'
+import { check, gen } from 'tape-check'
+import state from './state.mjs'
 
 test(
-  "Calls listener on creation",
+  'Calls listener on creation',
   check(gen.any, (t, value) => {
-    t.plan(3);
+    t.plan(3)
 
     const onState = state(() => {
-      t.pass("Is being called on init");
-      return value;
-    });
+      t.pass('Is being called on init')
+      return value
+    })
 
     onState(
       tap((state) =>
         t.equals(
           state,
           value,
-          "The value set in init is available in listener",
-        ),
-      ),
-    );
-  }),
-);
+          'The value set in init is available in listener'
+        )
+      )
+    )
+  })
+)
 
 test(
-  "Computed state should stay updated",
+  'Computed state should stay updated',
   check(gen.int, (t, int) => {
-    t.plan(13);
+    t.plan(13)
 
     const onState = state(() => ({
-      value: int,
-    }));
+      value: int
+    }))
 
     // Should eventually include the computed property
     onState(
       tap((state) => {
-        t.equals(state.value, int, "Value is equal to the test value");
+        t.equals(state.value, int, 'Value is equal to the test value')
 
         if (isNotNil(state.computed)) {
           t.equals(
             int + 1,
             state.computed,
-            "Computed is one more than int eventually",
-          );
+            'Computed is one more than int eventually'
+          )
         }
-      }),
-    );
+      })
+    )
 
     onState((state) => ({
       ...state,
-      computed: state.value + 1,
-    }));
+      computed: state.value + 1
+    }))
 
     onState(
       tap((state) => {
         t.equals(
           int + 1,
           state.computed,
-          "Computed is one more than int eventually",
-        );
-      }),
-    );
+          'Computed is one more than int eventually'
+        )
+      })
+    )
 
     onState((state, push) => {
       setTimeout(
         () =>
           push(
             tap((state) => {
-              t.pass("Push can be called even when listener is stopped");
-            }),
+              t.pass('Push can be called even when listener is stopped')
+            })
           ),
-        10,
-      );
-      push();
+        10
+      )
+      push()
 
-      return state;
-    });
-  }),
-);
+      return state
+    })
+  })
+)
