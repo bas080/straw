96b576bf49e2ea5a9f5df1d359d10cf993d61972
bas081 <bas080@hotmail.com>
2023-12-10 19:19:55 +0100

    Make the browser code work for OCaml HTML output

--- browser/issues.mjs
+++ browser/issues.mjs
@@ -1,82 +1,28 @@
-import { html, render } from "lit-html";
-import { onChange } from "./state.mjs";
-import { isEmpty, uniq } from "./helpers.mjs";
-
-function mapFocusable(elem, cb) {
-  for (const focusable of elem.querySelectorAll("a")) {
-    cb(focusable);
+function mapFocusable (elem, cb) {
+  for (const focusable of elem.querySelectorAll('a')) {
+    cb(focusable)
   }
 }
 
-export default function issues(onState) {
-  const issues = Array.from(document.getElementsByClassName("issue-issue"));
-  const index = issues.map((issue) => [issue, issue.textContent]);
-
-  const issuesPerToken = (tokens) => {
-    tokens = uniq(tokens);
-    return index.reduce((acc, [, content]) => {
-      tokens.forEach((token) => {
-        acc[token] = (acc[token] || 0) + Number(content.includes(token));
-      });
-      return acc;
-    }, {});
-  };
-
-  const onQueryChange = onChange();
-
-  // Most of the code in onState should only be run if tokens has changed.
+export default function issues (onState) {
   onState((state, push) => {
-    const { tokens } = state;
-
-    // Push computed state when the query changes.
-    onQueryChange(state.query, (a, b) => {
-      push((state) => {
-        state.issuesPerToken = issuesPerToken(tokens);
-
-        return state;
-      });
-    });
-
-    const ors = tokens.reduce(
-      (acc, token) => {
-        if (token === "or") return [[], ...acc];
-
-        acc[0].push(token);
-
-        return acc;
-      },
-      [[]],
-    );
-
-    function fuzzy(text) {
-      let matched;
-
-      return ors.some((tokens) =>
-        tokens.every((token) => text.includes(token)),
-      );
-    }
-
-    state.matchedCount = 0;
-    state.issuesCount = 0;
-    index.forEach(([elem, content]) => {
-      state.issuesCount += 1;
-      if (isEmpty(state.tokens) || fuzzy(content)) {
-        state.matchedCount += 1;
-        elem.classList.remove("disabled");
+    state.issues.forEach((elem) => {
+      if (state.matchedIssueElements.includes(elem)) {
+        elem.classList.remove('disabled')
         mapFocusable(elem, (focusable) => {
-          focusable.removeAttribute("tabindex");
-        });
+          focusable.removeAttribute('tabindex')
+        })
       } else {
         mapFocusable(elem, (focusable) => {
-          focusable.setAttribute("tabindex", "-1");
-        });
-        elem.classList.add("disabled");
+          focusable.setAttribute('tabindex', '-1')
+        })
+        elem.classList.add('disabled')
       }
-    });
+    })
 
-    document.getElementById("issue-search-count").innerText =
-      `Matched ${state.matchedCount} / ${state.issuesCount}`;
+    document.getElementById('issue-search-count').innerText =
+      `Matched ${state.matchedIssueElements.length} / ${state.issues.length}`
 
-    return state;
-  });
+    return state
+  })
 }
--- browser/state.mjs
+++ browser/state.mjs
@@ -1,56 +1,55 @@
-import { identity, excludeIndex, notEquals, isEmpty } from "./helpers.mjs";
+import { identity, excludeIndex, notEquals } from './helpers.mjs'
 
 const state = (initial) => {
-  let registered = [];
-  let oldState = initial();
-  let queue = true;
+  let registered = []
+  let oldState = initial()
+  let queue = true
 
-  return function register(onState) {
+  return function register (onState) {
     // Creates a unique reference for this function.
-    const internalOnState = (state) => onState(state, pushState);
-    const index = registered.length;
+    const internalOnState = (state) => onState(state, pushState)
+    const index = registered.length
 
-    registered.push(internalOnState);
-    pushState(identity);
+    registered.push(internalOnState)
+    pushState(identity)
 
-    function pushState(...args) {
+    function pushState (...args) {
       // Removes the listener when no arguments are pushed.
       if (args.length === 0) {
-        registered = excludeIndex(index, registered);
-        return;
+        registered = excludeIndex(index, registered)
+        return
       }
 
-      const [newValue] = args;
+      const [newValue] = args
 
-      oldState = newValue(oldState);
+      oldState = newValue(oldState)
 
       if (queue) {
         queueMicrotask(() => {
           oldState = registered.reduce(
             (acc, fn) => fn(acc),
-            newValue(oldState),
-          );
-          oldState = registered.reduce((acc, fn) => fn(acc), oldState);
-          queue = true;
-        });
-        queue = false;
+            newValue(oldState)
+          )
+          oldState = registered.reduce((acc, fn) => fn(acc), oldState)
+          queue = true
+        })
+        queue = false
       }
     }
-  };
-};
+  }
+}
 
 // helpers
-const firstCallSymbol = Symbol("firstCall");
+const firstCallSymbol = Symbol('firstCall')
 
 const onChange = (hasChanged = notEquals) => {
-  let old = firstCallSymbol;
+  let old = firstCallSymbol
   return (value, cb) => {
-    if (hasChanged(value, old) || old === firstCallSymbol)
-      cb(value, old === firstCallSymbol ? value : old);
+    if (hasChanged(value, old) || old === firstCallSymbol) { cb(value, old === firstCallSymbol ? value : old) }
 
-    old = value;
-  };
-};
+    old = value
+  }
+}
 
-export default state;
-export { onChange };
+export default state
+export { onChange }
