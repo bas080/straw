0de1373621f11569409601ba9c52848833def6a3
Antonis Kalou <kalouantonis@protonmail.com>
2023-12-16 18:30:19 +0200

    Handle Concat for title generation
    Skip empty link or text when searching for title
    
    Handle Omd.Concat for title generation

--- lib/issue_lib/omd_ext.ml
+++ lib/issue_lib/omd_ext.ml
@@ -1,48 +1,51 @@
 module Block = struct
   (* taken from https://github.com/ocaml/omd/blob/373e3f80e48001a62daf4072373ad3aa589ca65f/src/ast_block.ml#L62 *)
-  let rec map ~f =
+  let rec map ~f ?(concat = true) =
+    let open Omd in
     (* wrap f to handle Concat *)
     let rec f' = function
-      | Omd.Concat (attr, xs) ->
-        Omd.Concat (attr, List.map f' xs)
+      | Concat (attr, xs) as c ->
+        if concat then Concat (attr, List.map f' xs) else f c
       | _ as inline -> f inline
     in
     function
-    | Omd.Paragraph (attr, x) -> Omd.Paragraph (attr, f' x)
-    | Omd.List (attr, ty, sp, bl) ->
-      Omd.List (attr, ty, sp, List.map (List.map (map ~f:f')) bl)
-    | Omd.Blockquote (attr, xs) -> Omd.Blockquote (attr, List.map (map ~f:f') xs)
-    | Omd.Heading (attr, level, text) -> Omd.Heading (attr, level, f' text)
-    | Omd.Definition_list (attr, l) ->
-      let elt { Omd.term; defs } =
-        { Omd.term = f' term; defs = List.map f' defs }
+    | Paragraph (attr, x) -> Paragraph (attr, f' x)
+    | List (attr, ty, sp, bl) ->
+      List (attr, ty, sp, List.map (List.map (map ~f:f')) bl)
+    | Blockquote (attr, xs) -> Blockquote (attr, List.map (map ~f:f') xs)
+    | Heading (attr, level, text) -> Heading (attr, level, f' text)
+    | Definition_list (attr, l) ->
+      let elt { term; defs } =
+        { term = f' term; defs = List.map f' defs }
       in
       Definition_list (attr, List.map elt l)
-    | Omd.Table (attr, headers, rows) ->
+    | Table (attr, headers, rows) ->
       Table
         ( attr
         , List.map (fun (header, alignment) -> (f' header, alignment)) headers
         , List.map (List.map f') rows)
     (* we dont process the rest *)
-    | Omd.Thematic_break _ | Omd.Code_block _ | Omd.Html_block _ as x -> x
+    | Thematic_break _ | Code_block _ | Html_block _ as x -> x
 
-  let rec find_map ~f =
+  let rec find_map ~f ?(concat = true) =
+    let open Omd in
     let rec f' = function
-      | Omd.Concat (_, xs) -> List.find_map f' xs
+      | Concat (_, xs) as c ->
+        if concat then List.find_map f' xs else f c
       | _ as inline -> f inline
     in
     function
-    | Omd.Paragraph (_, x) | Omd.Heading (_, _, x) -> f' x
-    | Omd.List (_, _, _, xs) ->
+    | Paragraph (_, x) | Heading (_, _, x) -> f' x
+    | List (_, _, _, xs) ->
       List.find_map (List.find_map (find_map ~f:f')) xs
-    | Omd.Blockquote (_, xs) ->
+    | Blockquote (_, xs) ->
       List.find_map (find_map ~f:f') xs
     (* TODO: definiton_list, table *)
     | _ -> None
 
   let find ~f = find_map ~f:(fun x -> if f x then Some x else None)
 
-  let iter ~f block = map ~f:(fun x -> f x; x) block |> ignore
+  let iter ~f ?concat block = map ?concat ~f:(fun x -> f x; x) block |> ignore
 end
 
 module Document = struct
@@ -52,7 +55,7 @@ module Document = struct
   let find_map ~f = List.find_map f
 end
 
-let inline_map ~f = Document.map ~f:(Block.map ~f)
-let inline_iter ~f = Document.iter ~f:(Block.iter ~f)
-let inline_find ~f = List.find_map (Block.find ~f)
-let inline_find_map ~f = Document.find_map ~f:(Block.find_map ~f)
+let inline_map ~f ?concat = Document.map ~f:(Block.map ~f ?concat)
+let inline_iter ~f ?concat = Document.iter ~f:(Block.iter ~f ?concat)
+let inline_find ~f ?concat = List.find_map (Block.find ~f ?concat)
+let inline_find_map ~f ?concat = Document.find_map ~f:(Block.find_map ~f ?concat)
