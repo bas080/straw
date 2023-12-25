085d5eed8ebc079ab25483088985cd15572d5dcf
Antonis Kalou <kalouantonis@protonmail.com>
2023-12-25 19:02:22 +0200

    Rename project to straw

--- lib/issue_lib/cli.mli
+++ lib/issue_lib/cli.mli
@@ -1,7 +1,7 @@
-val issue_dir : unit -> Path.t
+val straw_dir : unit -> Path.t
 val init : unit -> unit
 val list : unit -> unit
-val open_issue : unit -> unit
+val open_item : unit -> unit
 val search : string -> unit
 val status : unit -> unit
 val html : unit -> unit
